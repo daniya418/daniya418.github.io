84d36fe553f3c1e5c3f9ca4d2db6d497943a5f3a
daniya418 <101710651+daniya418@users.noreply.github.com>
2022-12-11 21:51:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*================= typing animation ================= */
+
+var typed = new Typed(".typing",{
+    strings:["","Software Developer","Web Developer","Game Developer","Music Producer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
